52bcf94737c4d14169b9bc82ca9a61cb0c34e00d
XJY <3323048008@qq.com>
2025-05-26 15:19:30 +0800

    除gateway服务data platform服务，所有服务已经能够运行并部署

--- Tourlink_TravelPlatform_Backend/data-platform-service/src/main/java/org/tourlink/dataplatformservice/DataPlatformServiceApplication.java
+++ Tourlink_TravelPlatform_Backend/data-platform-service/src/main/java/org/tourlink/dataplatformservice/DataPlatformServiceApplication.java
@@ -2,8 +2,10 @@ package org.tourlink.dataplatformservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DataPlatformServiceApplication {
 
     public static void main(String[] args) {
