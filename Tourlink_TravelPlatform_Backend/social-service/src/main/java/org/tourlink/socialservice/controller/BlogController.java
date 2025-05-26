48515abaa53bd3b8f1c58a64f3bbb32fb1dc5afb
骏超 罗 <luojunchao@tongji.edu.cn>
2025-05-26 21:15:18 +0800

    网关测试

--- Tourlink_TravelPlatform_Backend/social-service/src/main/java/org/tourlink/socialservice/controller/BlogController.java
+++ Tourlink_TravelPlatform_Backend/social-service/src/main/java/org/tourlink/socialservice/controller/BlogController.java
@@ -17,7 +17,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/blogs")
+@RequestMapping("/api/social/blogs")
 public class BlogController {
 
     private final BlogService blogService;
