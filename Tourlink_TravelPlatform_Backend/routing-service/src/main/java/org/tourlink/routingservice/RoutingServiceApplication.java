6a70da360d3b7eba6f6839512d00b6a3aba1ebfc
骏超 罗 <luojunchao@tongji.edu.cn>
2025-06-10 20:52:57 +0800

    用户标签收集方式修改

--- Tourlink_TravelPlatform_Backend/routing-service/src/main/java/org/tourlink/routingservice/RoutingServiceApplication.java
+++ Tourlink_TravelPlatform_Backend/routing-service/src/main/java/org/tourlink/routingservice/RoutingServiceApplication.java
@@ -2,7 +2,10 @@ package org.tourlink.routingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
 @SpringBootApplication
+@EnableFeignClients
 public class RoutingServiceApplication {
 
     public static void main(String[] args) {
--- Tourlink_TravelPlatform_Backend/routing-service/src/main/java/org/tourlink/routingservice/controller/PathPlanController.java
+++ Tourlink_TravelPlatform_Backend/routing-service/src/main/java/org/tourlink/routingservice/controller/PathPlanController.java
@@ -3,18 +3,44 @@ package org.tourlink.routingservice.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.tourlink.common.dto.dataPlatformDTO.UserProfileDTO;
+import org.tourlink.routingservice.client.DataPlatformClient;
 import org.tourlink.routingservice.dto.PathPlanRequest;
 import org.tourlink.routingservice.dto.PathPlanResponse;
+import org.tourlink.routingservice.entity.UserPreference;
 import org.tourlink.routingservice.service.PathPlanningService;
+
+import java.util.Collections;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/path")
 public class PathPlanController {
 
     @Autowired
     private PathPlanningService pathPlanningService;
+
+    @Autowired
+    private DataPlatformClient dataPlatformClient;
+
     @PostMapping("/plan")
     public ResponseEntity<PathPlanResponse> planRoute(@RequestBody PathPlanRequest request) {
+
+        // 1. 调用数据中台接口获取用户标签权重
+        UserProfileDTO userProfile = dataPlatformClient.getUserProfile(request.getUserId());
+        Map<String, Double> tagWeights = (userProfile != null && userProfile.getTagWeights() != null)
+                ? userProfile.getTagWeights()
+                : Collections.emptyMap();
+
+        // 2. 设置到 UserPreference 中
+        if (request.getUserPreference() == null) {
+            request.setUserPreference(new UserPreference());
+        }
+        request.getUserPreference().setTagWeights(tagWeights);
+
+        // 3. 调用路径规划服务
         PathPlanResponse result = pathPlanningService.planRoute(request);
+
         return ResponseEntity.ok(result);
     }
 }
